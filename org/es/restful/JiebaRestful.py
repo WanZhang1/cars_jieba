46bdcba2d8ff76bc8586b3539df08cf4a48059f1
mick.yi <mick.yi@embracesource.com>
2017-11-21 16:19:03 +0800

    增加太原局货运站点字典

--- org/es/restful/JiebaRestful.py
+++ org/es/restful/JiebaRestful.py
@@ -20,7 +20,9 @@ jieba.load_userdict("../../../file/railDictionary.dat")
 # 铁路客运站点
 jieba.load_userdict("../../../file/stationDictionary.dat")
 # 货运线路
-jieba.load_userdict("../../../file/freigtLine.dat")
+jieba.load_userdict("../../../file/freightLine.dat")
+# 货运站点
+jieba.load_userdict("../../../file/freightStation.dat")
 
 #单个文本
 @app.route('/jieba_parse', methods=['POST'])
