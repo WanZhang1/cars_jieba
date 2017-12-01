e9e81c8b0626caa5cd44d0e2556cb620c6c45770
wendy.zhang <wendy.zhang@embracesource.com>
2017-12-01 14:39:48 +0800

    增加词语到词典

--- org/es/restful/JiebaRestful.py
+++ org/es/restful/JiebaRestful.py
@@ -5,24 +5,75 @@ from flask import Flask, abort, request, jsonify
 import jieba
 import jieba.posseg as pseg
 import os
+import sys
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
 
 app = Flask(__name__)
 
 # 自定义词典
-jieba.load_userdict("../../../file/accidentDepart.dat")
-jieba.load_userdict("../../../file/accidentGrade.dat")
-jieba.load_userdict("../../../file/accidentLocale.dat")
-jieba.load_userdict("../../../file/accidentName.dat")
-jieba.load_userdict("../../../file/originAccidentLocale.dat")
+jieba.load_userdict("dict/accidentDepart.dat")
+jieba.load_userdict("dict/accidentName.dat")
+jieba.load_userdict("dict/originAccidentLocale.dat")
 # 铁路专业词典
-jieba.load_userdict("../../../file/general.dat")
-jieba.load_userdict("../../../file/railDictionary.dat")
+jieba.load_userdict("dict/general.dat")
+jieba.load_userdict("dict/railDictionary.dat")
 # 铁路客运站点
-jieba.load_userdict("../../../file/stationDictionary.dat")
+jieba.load_userdict("dict/stationDictionary.dat")
 # 货运线路
-jieba.load_userdict("../../../file/freightLine.dat")
+jieba.load_userdict("dict/freightLine.dat")
 # 货运站点
-jieba.load_userdict("../../../file/freightStation.dat")
+jieba.load_userdict("dict/freightStation.dat")
+
+# 事故专业词典
+jieba.load_userdict("dict/locale_ok.dat")
+jieba.load_userdict("dict/name_ok.dat")
+jieba.load_userdict("dict/reason_ok.dat")
+jieba.load_userdict("dict/depart_ok.dat")
+
+
+
+# 添加词语到词典，可以添加多个，用 | 分隔开
+@app.route('/dict_add', methods=['POST'])
+def dict_add():
+	if not request.json or 'words' not in request.json or 'file' not in request.json:
+		abort(400)
+	words = request.json['words']
+	file = request.json['file']
+	
+	f = open("dict/"+file, 'a')
+	w = words.split("|")
+	
+	for i in range(0,len(w)):
+		f.write("\n"+w[i])
+	
+	f.close()
+	
+	return "Success"
+
+
+# 从词典删除词语,删除单个词语
+@app.route('/dict_del', methods=['POST'])
+def dict_del():
+	if not request.json or 'word' not in request.json or 'file' not in request.json:
+		abort(400)
+	word = request.json['word']
+	file = request.json['file']
+	
+	
+	f= open("dict/"+file, "r")
+	lines = f.readlines()
+	f_w=open("dict/"+file, "w")
+	for line in lines:
+		if word in line:
+			print(word + "," + line)
+			continue
+		f_w.write(line)
+	
+	f.close()
+	f_w.close()
+	
+	return "Success"
 
 #单个文本
 @app.route('/jieba_parse', methods=['POST'])
